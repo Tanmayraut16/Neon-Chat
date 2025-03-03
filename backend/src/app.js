9aa45de7a7f8ec6edd956f96cc3781c932c590c9
Tanmayraut16 <Tanmayraut16@outlook.com>
2025-03-03 23:33:54 +0530

    Add .gitignore, update package.json scripts, and configure production settings for Express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 import { app } from "./utils/socket.js";
 
 app.use(
@@ -20,9 +22,19 @@ app.use(cookieParser());
 import authRouter from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
 
+const __dirname = path.resolve();
+
 //routes declaration
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/messages", messageRouter);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  });
+}
 
 // http://localhost:8000/api/v1/users/register
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import {app} from './app.js'
 import { server } from "./utils/socket.js";
-dotenv.config({
-    path: './.env'
-})
-
-
+dotenv.config()
 
 connectDB()
 .then(() => {
